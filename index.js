8533bc7dd75c13c74efd90bdff4d0c86e7739226
willk0814 <wkoenig0814@gmail.com>
2024-07-08 13:53:36 -0400

    Changed listening message

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-
 const port = 5001
-app.listen(port, () => console.log(`Api running on http://localhost:${port}/`));
+app.listen(port, () => console.log(`Listening at http://localhost:${port}/`));
 app.get('/', (req, res) => res.json('Hello World'));
